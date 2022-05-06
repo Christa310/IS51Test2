4b9721edd40df018ad96ce5cd63ad13f52095796
Christa <Christa21819@gmail.com>
2022-05-05 20:48:26 -0700

    structured english

---
+++ IS51_Test2.py
@@ -0,0 +1,12 @@
+"""
+
+To start we will gather the grades from the Final.txt file and 
+catputure users input for each grade. Each time we capture the users input,
+we append the number to the list. once we have three highest number in the list, 
+we sum them up and divide by 3 and output a message to the user.
+We will display the number of grades, the average grade, 
+and a calculate_percent_above_average to calculate the percentage 
+of grades that are above the average grade.
+
+"""
+
